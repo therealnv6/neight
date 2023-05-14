a855a0683af191705dd67e044d8d4de37fe3f261
therealnv6 <devrawr@protonmail.com>
2023-05-14 10:19:47 +0200

    update documentation

--- include/memory.h
+++ include/memory.h
@@ -7,31 +7,31 @@ namespace emu
     typedef std::array<unsigned char, 4096> memory_alloc;
 
     /*
-    Memory Map:
-    +---------------+= 0xFFF (4095) End of Chip-8 RAM
-    |               |
-    |               |
-    |               |
-    |               |
-    |               |
-    | 0x200 to 0xFFF|
-    |     Chip-8    |
-    | Program / Data|
-    |     Space     |
-    |               |
-    |               |
-    |               |
-    +- - - - - - - -+= 0x600 (1536) Start of ETI 660 Chip-8 programs
-    |               |
-    |               |
-    |               |
-    +---------------+= 0x200 (512) Start of most Chip-8 programs
-    | 0x000 to 0x1FF|
-    | Reserved for  |
-    |  interpreter  |
-    +---------------+= 0x000 (0) Start of Chip-8 RAM
-
-    */
+     * Memory Map:
+     * +---------------+= 0xFFF (4095) End of Chip-8 RAM
+     * |               |
+     * |               |
+     * |               |
+     * |               |
+     * |               |
+     * | 0x200 to 0xFFF|
+     * |     Chip-8    |
+     * | Program / Data|
+     * |     Space     |
+     * |               |
+     * |               |
+     * |               |
+     * +- - - - - - - -+= 0x600 (1536) Start of ETI 660 Chip-8 programs
+     * |               |
+     * |               |
+     * |               |
+     * +---------------+= 0x200 (512) Start of most Chip-8 programs
+     * | 0x000 to 0x1FF|
+     * | Reserved for  |
+     * |  interpreter  |
+     * +---------------+= 0x000 (0) Start of Chip-8 RAM
+     *
+     */
     class memory
     {
     public:
